6ea753d311200988540184fa660ce4ba90921b0b
천현민 <poppy3672@naver.com>
2023-05-16 14:12:43 +0900

    JSX

---
+++ project03/src/Ex02.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import picSrc from './img/likePic1.gif'
+
+const Ex02 = () => {
+
+/* 
+    React에서 내부 이미지 경로 지정하는 방법
+
+    1) src 폴더에서 이미지를 import하기
+        -import 이미지변수 from '경로'
+        -<img src={이미지변수}/>
+
+    2)public 폴더 내에 이미지를 저장하고 접근하기
+        -public - img 폴더를 만든 후 파일을 보관
+        - 이 때, 별도로 import 는 필요하지 않다.
+            why? public 폴더는 서버와 계속해서 통신하기 때문
+*/
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default Ex02
