adb59b6215256e472b28d2c8a16121a385a990ee
천현민 <poppy3672@naver.com>
2023-05-23 09:35:34 +0900

    teacher answwer

--- project05/src/components/Ex03Header.jsx
+++ project05/src/components/Ex03Header.jsx
@@ -3,9 +3,12 @@ import { useContext } from 'react'
 import { DarkContext } from '../context/Ex03DarkContext'
 
 const Ex03Header = () => {
-  
+  const{isDark} =useContext(DarkContext)
   return (
-    <div>
+    <div style={{
+      backgroundColor : isDark ? "black" : "white",
+      color : isDark ? "white" : "black"
+    }}>
       으으으응
     </div>
   )
--- project05/src/components/Ex03Main.jsx
+++ project05/src/components/Ex03Main.jsx
@@ -3,26 +3,46 @@ import { useContext } from 'react'
 import { DarkContext } from '../context/Ex03DarkContext'
 
 const Ex03Main = () => {
-  const{IsDark, setIsDark, setTextColor, setDarkColor} = useContext(DarkContext)
-  return (
-    <div>
 
-      <button onClick={()=>{
-        IsDark == false ?  setIsDark(true) : setIsDark (false);
-        if(IsDark){
-          setTextColor ("black");
-          setDarkColor ("white");
-        }
-        else{
-          
-          setTextColor("white");
-          setDarkColor ("black");
-        }
+  const{isDark, setIsDark}=useContext(DarkContext)
+  
+  const toggleDark =()=>{
+    setIsDark(!isDark)
+    console.log(isDark);
+  }
 
-      }}
-      >다크모드</button>
+  return(
+    
+    <div style={{
+      backgroundColor : isDark ? "black" : "white",
+      color : isDark ? "white" : "black"
+    }}>
+      Ex03Main
+      <br/>
+      <button onClick={toggleDark}>다크모드</button>
     </div>
   )
 }
 
 export default Ex03Main
+
+// const{IsDark, setIsDark, setTextColor, setDarkColor} = useContext(DarkContext)
+//   return (
+//     <div>
+
+//       <button onClick={()=>{
+//         IsDark == false ?  setIsDark(true) : setIsDark (false);
+//         if(IsDark){
+//           setTextColor ("black");
+//           setDarkColor ("white");
+//         }
+//         else{
+          
+//           setTextColor("white");
+//           setDarkColor ("black");
+//         }
+
+//       }}
+//       >다크모드</button>
+//     </div>
+//   )
