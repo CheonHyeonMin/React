00abc6ae87a4b7a05ca5db0c0e2ed689b0ce47a3
천현민 <poppy3672@naver.com>
2023-05-28 21:00:50 +0900

    asdf

--- practice00/src/components/Ex00AddTodo.jsx
+++ practice00/src/components/Ex00AddTodo.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { useContext } from 'react'
-import { TodoContext } from '../context/Ex00TodoContext'
+import React from "react";
+import { useContext } from "react";
+import { TodoContext } from "../context/Ex00TodoContext";
 
 const Ex00AddTodo = () => {
-    const {newTodos,setnewTodos,handleTodoAddition} = useContext(TodoContext);
+  const { newTodos, handleTodoAddition, handleNewTodo } =
+    useContext(TodoContext);
   return (
     <div>
-        <input type="text" onChange={(e)=>{
-            setnewTodos(e.target.value)
-            console.log(newTodos);
-        }}/>
-        <button onClick={handleTodoAddition}>추가</button>
+      <input type="text" onChange={handleNewTodo} value={newTodos} />
+      <button onClick={handleTodoAddition}>추가</button>
     </div>
-  )
-}
+  );
+};
 
-export default Ex00AddTodo
+export default Ex00AddTodo;
